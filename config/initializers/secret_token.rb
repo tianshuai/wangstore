93e863ccad416938782d8e94369b5d16e5af7d51
tianshuai <tianshuaiorc@sina.com>
2013-09-11 15:06:41 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wangstore::Application.config.secret_key_base = '642be851185e494a90635e1f2177b3b8bb046b06384be44cdf564e845904e999e5c3b34c9dce098a94a5f1af37798a88fb339026a4767050dca6a1d050f26994'
